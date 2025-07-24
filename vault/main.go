201d719badea7e7704a2dd37fe8e8c5ee51d042f
Emmanuel Bachlitzanakis <115871174+EmBachlitzanakis@users.noreply.github.com>
2025-07-24 12:51:37 +0300

    Add files via upload

--- vault/main.go
+++ vault/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/ebachlitzanakis/vault/internal/crypto"
@@ -49,33 +47,4 @@ func main() {
 	// Initialize and start server
 	srv := server.NewServer(port, passwordHandler)
 	log.Fatal(srv.Start())
-}
-
-func main() {
-	// Serve static files (index.html, etc.)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/" {
-			http.ServeFile(w, r, "index.html")
-			return
-		}
-		// Serve other static assets if any, though not strictly needed for this example
-		http.ServeFile(w, r, r.URL.Path[1:])
-	})
-
-	// API endpoints
-	http.HandleFunc("/passwords", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			getPasswordsHandler(w, r)
-		case http.MethodPost:
-			addPasswordHandler(w, r)
-		default:
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		}
-	})
-	http.HandleFunc("/passwords/", deletePasswordHandler) // For DELETE requests with ID
-
-	port := 8080
-	log.Printf("Server starting on :%d", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
